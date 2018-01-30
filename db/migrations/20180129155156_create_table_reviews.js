3d40f79b3e6220ee8f1f2d1382a7a53d29f31d4e
NickGatti <nick.gatti@gmail.com>
2018-01-30 09:58:42 -0700

    init

---
+++ db/migrations/20180129155156_create_table_reviews.js
@@ -0,0 +1,17 @@
+exports.up = function ( knex, Promise ) {
+    return knex.schema.createTable( 'reviews', ( table ) => {
+        table.increments()
+        table.integer( 'appearance' ).notNullable()
+        table.integer( 'taste' ).notNullable()
+        table.integer( 'smell' ).notNullable()
+        table.integer( 'effect' ).notNullable()
+        table.integer( 'reccomend' ).notNullable()
+        table.string( 'from' )
+        table.string( 'share' )
+        table.timestamps( true, true )
+    } )
+};
+
+exports.down = function ( knex, Promise ) {
+    return knex.schema.dropTable( 'reviews' )
+};
