cf052162252c209bc6de2f9c1b8ce1c93ff2d765
NickGatti <nick.gatti@gmail.com>
2018-01-31 12:29:29 -0700

    we will only add reviews through postico

--- db/seeds/04_reviews.js
+++ db/seeds/04_reviews.js
@@ -4,15 +4,6 @@ exports.seed = function ( knex, Promise ) {
         .then( function () {
             // Inserts seed entries
             return knex( 'reviews' ).insert( [
-                {
-                    appearance: 7,
-                    taste: 7,
-                    smell: 7,
-                    effect: 7,
-                    recommend: 7,
-                    from: 'Level Up dispensary',
-                    share: 'Facebook'
-                }
               ] );
         } );
 };
