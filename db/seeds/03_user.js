9ba99145c7278b2b540f8717e098dc8ad99f283c
Jennifer Tristan <jt6161@cox.net>
2018-01-30 21:14:29 -0700

    folders

--- db/seeds/03_user.js
+++ db/seeds/03_user.js
@@ -1,17 +1,17 @@
-exports.seed = function ( knex, Promise ) {
-    // Deletes ALL existing entries
-    return knex( 'reviews' ).del()
-        .then( function () {
-            // Inserts seed entries
-            return knex( 'reviews' ).insert( [
-                {
-                    appearance: 7,
-                    taste: 7,
-                    smell: 7,
-                    effect: 7,
-                    recommend: 7,
-                    from: 'Level Up dispensary',
-                    share: 'facebook'
-                } ] );
-        } );
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('reviews').del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('reviews').insert([
+        {
+          appearance: 7,
+          taste: 7,
+          smell: 7,
+          effect: 7,
+          recommend: 7,
+          from: 'Level Up dispensary',
+          share: 'Facebook'
+        }]);
+    });
 };
