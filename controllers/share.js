e4f525d63f87576dd5f0a220fb9f72672587b543
NickGatti <nick.gatti@gmail.com>
2018-02-01 12:03:26 -0700

    product search

--- controllers/share.js
+++ controllers/share.js
@@ -3,25 +3,25 @@ const knex = require( "../db/knex.js" );
 module.exports = {
 
     view: function ( req, res, next ) {
-        if ( !req.session.reviews ) {
+        if ( !req.session.products ) {
             res.render( 'share', {
-                reviews: false
+                products: false
             } )
             return
         }
         res.render( 'share', {
-            reviews: req.session.reviews
+            products: req.session.products
         } )
     },
 
     search: function ( req, res, next ) {
-        knex( 'reviews' )
+        knex( 'product' )
             .where( { 'name': req.body.search } )
             .orWhere( { 'brand': req.body.search } )
             .orWhere( { 'category': req.body.search } )
             .orWhere( { 'description': req.body.search } )
-            .then( ( reviewData ) => {
-                req.session.reviews = reviewData
+            .then( ( productData ) => {
+                req.session.products = productData
                 req.session.save( () => {
                     res.redirect( '/share' )
                 } )
