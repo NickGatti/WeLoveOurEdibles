82559ea90e193a61b12eee10c07b7618eb7056cd
NickGatti <nick.gatti@gmail.com>
2018-02-01 22:37:12 -0700

    fixed routes and added some stuff to pages

--- db/seeds/02_product.js
+++ db/seeds/02_product.js
@@ -5,73 +5,73 @@ exports.seed = function ( knex, Promise ) {
             // Inserts seed entries
             return knex( 'product' ).insert( [ {
                     name: 'Chocolate Fudge Brownie',
-                    brand: 'Super Haaaa',
+                    brand: 'Super Haaa',
                     MMJ: true,
                     category: 'Baked',
                     description: 'Super Haaa Super Baked Chocolate Fudge Brownie. Flaky and chewy come get super haaa with us!',
                     img_url: '/images/baked1.jpg',
                 }, {
-                    name: 'Baked 2',
-                    brand: 'Baked 2 Brand',
+                    name: 'Double Chocolate Chip Cookie',
+                    brand: 'Half Caked',
                     MMJ: true,
                     category: 'Baked',
-                    description: 'Baked One',
+                    description: 'Half Caked Double Chocolate Chip Cookie. Chewy and delecious, this one sets in after you forgot it was even MMJ and youve had 5.',
                     img_url: '/images/baked2.jpg'
                 }, {
-                    name: 'Candy 1',
-                    brand: 'Candy 1 Brand',
+                    name: 'JK Bears',
+                    brand: 'Slipz',
                     MMJ: true,
                     category: 'Candy',
-                    description: 'Candy One',
+                    description: 'Slipz JK Bears. JK because these are the ones with 150mg you forgot you had in in your car last week and you just ate the entire bag!',
                     img_url: '/images/candy1.jpg'
                 }, {
-                    name: 'Candy 2',
-                    brand: 'Candy 2 Brand',
+                    name: 'Pecan Crunch',
+                    brand: 'NatraMMJ',
                     MMJ: true,
                     category: 'Candy',
-                    description: 'Candy Two',
+                    description: 'NartaMMJ Organic Pecan Cruch. These are so good it\'s really easy to eat too many.',
                     img_url: '/images/candy2.jpg'
                 }, {
-                    name: 'Candy 3',
-                    brand: 'Candy 3 Brand',
+                    name: 'Lux 90% Infusion',
+                    brand: 'True Chef',
                     MMJ: true,
                     category: 'Candy',
-                    description: 'Candy Three',
+                    description: 'True Chef 90% dark chocolate cacao infused with almond. Savory and sweet.',
                     img_url: '/images/candy3.jpg'
                 }, {
-                    name: 'Drink 1',
-                    brand: 'Drink 1 Brand',
+                    name: 'Via Tea Afterhours',
+                    brand: 'Via',
                     MMJ: true,
                     category: 'Drinks',
-                    description: 'Drink One',
+                    description: 'Via\'s Via Tea Afterhours, a sparkling blend of honey, chammomile mixed with our brew of MMJ tea.',
                     img_url: '/images/drinks1.jpg'
                 }, {
-                    name: 'Salted 1',
-                    brand: 'Salted 1 Brand',
+                    name: 'Puff Pops',
+                    brand: 'Puffys',
                     MMJ: true,
                     category: 'Salty Snacks',
-                    description: 'Salty Snack One',
+                    description: 'Puffys Puff Pops, lemon and garlic infused toasted corn. Great for a little bite.',
                     img_url: '/images/salted1.jpg'
                 }, {
-                    name: 'Salted 2',
-                    brand: 'Salted 2 Brand',
+                    name: 'Preztzel Zinng',
+                    brand: 'Puffys',
                     MMJ: true,
                     category: 'Salty Snacks',
-                    description: 'Salty Snack Two',
+                    description: 'Puffys take on traditional Japanese salted snacks, MMJ infused style.',
                     img_url: '/images/salted2.jpg'
                 }, {
-                    name: 'Sweets 1',
-                    brand: 'Sweets 1 Brand',
+                    name: 'W.T. Puffycock',
+                    brand: 'Puffys',
                     MMJ: true,
                     category: 'Candy',
-                    description: 'Sweet Things',
+                    description: 'Puffys W.T. Puffycock, a MMJ infused style of carmel corn.',
                     img_url: '/images/sweets1.jpg'
                 }, {
-                    name: 'Sweets 2',
-                    brand: 'Sweets 2 Brand',
+                    name: 'Pop bites',
+                    brand: 'Puffys',
                     MMJ: true,
                     category: 'Candy',
-                    description: 'Sweet Things',
+                    description: 'Puffys Pop Bites, the perfect switch from some Puff Pops, this guarantees full satisfaction.',
                     img_url: '/images/sweets2.jpg'
                 },
           ] );
