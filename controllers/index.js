f6bfc560a81e5937365acfb3a2ad07d9a9cdbf08
NickGatti <nick.gatti@gmail.com>
2018-02-02 10:54:29 -0700

    logout

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,12 @@ module.exports = {
 
     view: function ( req, res, next ) {
         res.redirect( '/home ' )
+    },
+
+    logout: function ( req, res, next ) {
+        delete req.session.user
+        delete req.session.admin
+        res.redirect( '/home ' )
     }
 
 }
